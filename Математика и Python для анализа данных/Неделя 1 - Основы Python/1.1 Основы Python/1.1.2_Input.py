8ae5d782f6cedc5daa1edae616ef6cd5c8763b8e
SergioKulyk <sergiokulyk@gmail.com>
2018-03-05 18:30:07 +0200

    add 1.1.2

---
+++ Математика и Python для анализа данных/Неделя 1 - Основы Python/1.1 Основы Python/1.1.2_Input.py
@@ -0,0 +1,11 @@
+# В данной задаче вам необходимо считать из потока ввода (консоль, с помощью input) имя, и поздороваться с человеком.
+#
+# Sample Input:
+#
+# Anna
+# Sample Output:
+#
+# Hello, Anna
+
+name = input()
+print("Hello, " + name)
