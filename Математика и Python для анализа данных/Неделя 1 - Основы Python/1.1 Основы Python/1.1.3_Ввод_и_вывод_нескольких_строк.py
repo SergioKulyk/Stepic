6c1905e0889390a55bd380fc5eadb95c90d34c2d
SergioKulyk <sergiokulyk@gmail.com>
2018-03-05 18:32:15 +0200

    add 1.1.3

---
+++ Математика и Python для анализа данных/Неделя 1 - Основы Python/1.1 Основы Python/1.1.3_Ввод_и_вывод_нескольких_строк.py
@@ -0,0 +1,17 @@
+# Считайте имена 2 людей и поздоровайтесь с ними.
+#
+# Sample Input:
+#
+# Anna
+# Aleksey
+# Sample Output:
+#
+# Hello, Anna
+# Hello, Aleksey
+
+# put your python code here
+name1 = input()
+name2 = input()
+
+print("Hello, " + name1)
+print("Hello, " + name2)
