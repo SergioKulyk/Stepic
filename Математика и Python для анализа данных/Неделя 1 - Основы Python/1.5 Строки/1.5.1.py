1da6142bffb97fa20407e64996c3ae30a173fc09
SergioKulyk <sergiokulyk@gmail.com>
2018-03-06 20:43:54 +0200

    add 1.5

---
+++ Математика и Python для анализа данных/Неделя 1 - Основы Python/1.5 Строки/1.5.1.py
@@ -0,0 +1,11 @@
+# Считайте текст с клавиатуры (из стандартного потока ввода) и выведите его на печать.
+#
+# Sample Input:
+#
+# Hello, World!
+# Sample Output:
+#
+# Hello, World!
+
+s = input()
+print(s)
---
+++ Математика и Python для анализа данных/Неделя 1 - Основы Python/1.5 Строки/1.5.2.py
@@ -0,0 +1,13 @@
+# Считайте 2 строки и выведите их на печать, разделив символом $.
+#
+# Sample Input:
+#
+# Uno
+# Dos
+# Sample Output:
+#
+# Uno$Dos
+
+s1 = input()
+s2 = input()
+print(s1, s2, sep='$')
